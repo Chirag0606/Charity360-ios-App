48ee11ae449758f808ff1b2f2d9e2e895aaae4eb
user1 <user1@cs2449.local>
2024-01-17 18:45:48 +0530

    created create event page

---
+++ charity360/Registration.swift
@@ -0,0 +1,17 @@
+//
+//  Registration.swift
+//  charity360
+//
+//  Created by user1 on 16/01/24.
+//
+
+import Foundation
+
+struct Registration {
+    var nameOfEvent: String
+    
+    var eventDate: Date
+    
+    var organization: String
+    var typeOfEvent: String
+}

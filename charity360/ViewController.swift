515824f3bb620c2ba00f8a5920b8849a04c62e35
user1 <user1@MacBook-Pro.local>
2024-02-21 15:45:48 +0530

    ui(95%)

--- charity360/ViewController.swift
+++ charity360/ViewController.swift
@@ -18,11 +18,29 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var yourEventsSegmentView: UIView!
     
+    @IBOutlet weak var account: UISegmentedControl!
+    
+    @IBOutlet weak var loginSegmentView: UIView!
+    
+    @IBOutlet weak var registerSegmentView: UIView!
+    
+    @IBOutlet weak var paymentField: UITextField!
+    
+    let methods = ["GPay", "PayTM", "PhonePe", "BHIM"]
+    
+    var methodPickerView = UIPickerView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.view.bringSubviewToFront(completedSegmentView)
+        
+        //paymentField?.textAlignment = .center
+        
+        paymentField?.inputView = methodPickerView
+        methodPickerView.delegate = self
+        methodPickerView.dataSource = self
+        
+        methodPickerView.tag = 1
     }
     
     
@@ -41,8 +59,54 @@ class ViewController: UIViewController {
     }
     
     
+    @IBAction func loginRegister(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            self.view.bringSubviewToFront(loginSegmentView)
+        case 1:
+            self.view.bringSubviewToFront(registerSegmentView)
+        default:
+            break
+        }
+    }
     
     
+}
+
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch pickerView.tag {
+        case 1:
+            return methods.count
+        default:
+            return 1
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch pickerView.tag {
+        case 1:
+          //
+            return methods[row]
+        default:
+            return "Data not found"
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //paymentField.text = pickerView.textInputContextIdentifier
+        switch pickerView.tag {
+        case 1:
+            paymentField.text = methods[row]
+            paymentField.resignFirstResponder()
+        default:
+            return
+        }
+    }
 }
 
 
